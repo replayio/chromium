f708add673f9bbbce803570a7527cbb6d2027da0
Brian Hackett <bhackett1024@gmail.com>
2022-09-19 08:52:50 -0700

    Remove lock annotations

--- base/record_replay.h
+++ base/record_replay.h
@@ -122,8 +122,8 @@ class SCOPED_LOCKABLE AutoLockMaybeEventsDisallowed {
 // events during the acquire to avoid generating a warning.
 class SCOPED_LOCKABLE AutoUnlockMaybeEventsDisallowed {
  public:
-  AutoUnlockMaybeEventsDisallowed(base::Lock& lock) UNLOCK_FUNCTION();
-  ~AutoUnlockMaybeEventsDisallowed() EXCLUSIVE_LOCK_FUNCTION();
+  AutoUnlockMaybeEventsDisallowed(base::Lock& lock);
+  ~AutoUnlockMaybeEventsDisallowed();
 
  private:
   base::Lock& lock_;
