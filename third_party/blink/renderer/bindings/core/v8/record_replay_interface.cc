34d9458aba17acf9c6fc5ebf833de0c0693faec6
Ryan Duffy <ryan@replay.io>
2022-11-09 11:12:08 -0800

    to v8 string

--- third_party/blink/renderer/bindings/core/v8/record_replay_interface.cc
+++ third_party/blink/renderer/bindings/core/v8/record_replay_interface.cc
@@ -1564,7 +1564,7 @@ static void InvokeOnAnnotation(const v8::FunctionCallbackInfo<v8::Value>& args) 
   v8::Isolate* isolate = args.GetIsolate();
   v8::Local<v8::Object> payload = v8::Object::New(isolate);
   v8::Local<v8::Context> context = isolate->GetCurrentContext();
-  payload->Set(context, "message", args[1]).Check();
+  payload->Set(context, ToV8String(isolate, "message"), args[1]).Check();
 
   v8::Local<v8::String> json;
   if (!v8::JSON::Stringify(context, payload).ToLocal(&json)) {
