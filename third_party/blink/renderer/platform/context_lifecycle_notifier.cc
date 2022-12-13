44c0a902a5f79c98d53c87e079e9497d85bd4797
Brian Hackett <bhackett1024@gmail.com>
2022-12-13 10:56:05 -0800

    Notify context destruction observers consistently

--- third_party/blink/renderer/platform/context_lifecycle_notifier.cc
+++ third_party/blink/renderer/platform/context_lifecycle_notifier.cc
@@ -19,24 +19,80 @@ ContextLifecycleNotifier::~ContextLifecycleNotifier() {
 void ContextLifecycleNotifier::AddContextLifecycleObserver(
     ContextLifecycleObserver* observer) {
   observers_.AddObserver(observer);
+
+  if (recordreplay::IsRecordingOrReplaying("values") && recordreplay::IsReplaying())
+    replay_observers_.push_back(observer);
 }
 
 void ContextLifecycleNotifier::RemoveContextLifecycleObserver(
     ContextLifecycleObserver* observer) {
   DCHECK(observers_.HasObserver(observer));
   observers_.RemoveObserver(observer);
+
+  for (auto it : replay_observers_) {
+    if (*it == observer) {
+      replay_observers_.erase(it);
+      break;
+    }
+  }
 }
 
 void ContextLifecycleNotifier::NotifyContextDestroyed() {
-  // The observers can vary when replaying as weak references are used and different
-  // observers may have been swept. Avoid interacting with the recording while we
-  // do the notifications.
-  recordreplay::AutoDisallowEvents disallow;
-
+  HeapVector<Member<ContextLifecycleObserver>> observers;
   observers_.ForEachObserver([](ContextLifecycleObserver* observer) {
-    observer->NotifyContextDestroyed();
+    observers.push_back(observer);
   });
   observers_.Clear();
+
+  if (recordreplay::IsRecordingOrReplaying("values") &&
+      !recordreplay::AreEventsDisallowed()) {
+    if (recordreplay::IsRecording()) {
+      size_t num_observers = observers.size();
+      recordreplay::RecordReplayValue("NotifyContextDestroyed NumObservers", num_observers);
+      int* observer_ids = new int[num_observers];
+      for (size_t i = 0; i < observers.size(); i++) {
+        int id = recordreplay::PointerId(observers[i]);
+        CHECK(id);
+        observer_ids[i] = id;
+      }
+      recordreplay::RecordReplayBytes("ContextLifecycleNotifier::NotifyContextDestroyed ObserverIds",
+                                      observer_ids, num_observers * sizeof(int));
+      delete[] observer_ids;
+    } else {
+      size_t num_observers = recordreplay::RecordReplayValue("NotifyContextDestroyed NumObservers", 0);
+      int* observer_ids = new int[num_observers];
+      recordreplay::RecordReplayBytes("ContextLifecycleNotifier::NotifyContextDestroyed ObserverIds",
+                                      observer_ids, num_observers * sizeof(int));
+
+      HeapVector<Member<ContextLifecycleObserver>> new_observers;
+      for (ContextLifecycleObserver* observer : observers) {
+        int id = recordreplay::PointerId(observer);
+        CHECK(id);
+        bool found = false;
+        for (int = 0; i < num_observers; i++) {
+          if (observer_ids[i] == id) {
+            found = true;
+            break;
+          }
+        }
+        if (found)
+          new_observers.push_back(observer);
+      }
+
+      observers = std::move(new_observers);
+      delete[] observer_ids;
+    }
+  }
+
+  for (ContextLifecycleObserver* observer : observers) {
+    if (!recordreplay::AreEventsDisallowed()) {
+      recordreplay::Assert("ContextLifecycleNotifier::NotifyContextDestroyed #1 %d",
+                           recordreplay::PointerId(observer));
+    }
+    observer->NotifyContextDestroyed();
+  }
+
+  replay_observers_.Clear();
 
 #if DCHECK_IS_ON()
   did_notify_observers_ = true;
@@ -45,6 +101,7 @@ void ContextLifecycleNotifier::NotifyContextDestroyed() {
 
 void ContextLifecycleNotifier::Trace(Visitor* visitor) const {
   visitor->Trace(observers_);
+  visitor->Trace(replay_observers_);
 }
 
 }  // namespace blink
