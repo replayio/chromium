10340a9fe18ec61e55b04f6a1ad0a4fd50322648
Domi <Domiii@users.noreply.github.com>
2023-01-07 02:04:35 +0800

    fixed copyright + license notices

--- build.js
+++ build.js
@@ -1,4 +1,6 @@
-// Copyright 2021 Record Replay Inc.
+// Copyright 2021 Record Replay Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 const fs = require("fs");
 const path = require("path");
--- uploadChromiumBuildToCloudDevelopment.sh
+++ uploadChromiumBuildToCloudDevelopment.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# Copyright 2021 Record Replay Inc.
+
+# Copyright 2021 Record Replay Inc. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # Use the current dir as reference point.
 DIR=$(pwd)
