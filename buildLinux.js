e8346f1e4b6654031cd10ff86ca90ddc940f068d
Dan Miller <dan@replay.io>
2023-03-07 12:30:12 -0800

    fix current working directory

--- buildLinux.js
+++ buildLinux.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const { spawnSync } = require("child_process");
 
-const chromium = process.cwd;
+const chromium = process.cwd();
 
 try {
   spawnChecked("git", ["pull"], { cwd: chromium, stdio: "inherit" });
