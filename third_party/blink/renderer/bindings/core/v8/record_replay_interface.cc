a0a12f9eb715663f5b060d122c161c2b87f0b177
Domi <Domiii@users.noreply.github.com>
2023-04-07 18:34:37 +0800

    WIP: fix native object checks without invoking user JS

--- third_party/blink/renderer/bindings/core/v8/record_replay_interface.cc
+++ third_party/blink/renderer/bindings/core/v8/record_replay_interface.cc
@@ -26,13 +26,13 @@
 #include "third_party/blink/renderer/core/inspector/inspector_css_agent.h"
 #include "third_party/blink/renderer/core/inspector/inspector_dom_agent.h"
 #include "third_party/blink/renderer/core/inspector/inspector_dom_debugger_agent.h"
-#include "third_party/blink/renderer/core/inspector/inspector_dom_debugger_agent.h"
 #include "third_party/blink/renderer/core/inspector/inspector_network_agent.h"
 #include "third_party/blink/renderer/core/inspector/inspector_resource_container.h"
 #include "third_party/blink/renderer/core/inspector/inspector_resource_content_loader.h"
 #include "third_party/blink/renderer/core/inspector/resolve_node.h"
 #include "third_party/blink/renderer/platform/bindings/script_forbidden_scope.h"
 #include "third_party/blink/renderer/platform/bindings/v8_binding.h"
+#include "third_party/blink/renderer/platform/bindings/v8_dom_wrapper.h"
 #include "third_party/inspector_protocol/crdtp/maybe.h"
 #include "v8/include/v8-inspector.h"
 
@@ -98,6 +98,7 @@ const {
   fromJsMakeDebuggeeValue,
   fromJsGetArgumentsInFrame,
   fromJsGetObjectByCdpId,
+  fromJsIsNativeObject,
   fromJsGetNodeId,
   fromJsGetBoxModel,
   fromJsGetMatchedStylesForNode,
@@ -561,39 +562,15 @@ function Graphics_getDevicePixelRatio() {
 // Utilities
 ///////////////////////////////////////////////////////////////////////////////
 
-/**
- * NOTE: this is not a guarantee, since `toString()` can be overridden.
- * NOTE2: v8 debugger has `CalculateNativeAccessorFlags` but is expensive and cached and annoying.
- */
-function isProbablyNativeFunction(f) {
-  return isNativeFunctionDescription(f.toString());
-}
-
-function isNativeFunctionDescription(functionString) {
-  return functionString?.endsWith('() { [native code] }') || false;
-}
-
 function isPrototype(x) {
   return x === x?.constructor?.prototype;
 }
 
-
 /**
- * Check whether given object `x` has what looks like a native ctor.
+ * Check whether given object `x` is a native object.
  */
-function isProbablyInstanceOfNativeClass(x) {
-  // hackfix: check if its ctor has a native toString
-  try {
-    return x?.constructor &&
-      // avoid false positives for prototypes (see https://linear.app/replay/issue/RUN-1067)
-      x.constructor === x.__proto__?.constructor &&
-      isProbablyNativeFunction(x.constructor);
-  }
-  catch (err) {
-    // Note: there can be some errors here, e.g.:
-    // "Error: Blocked a frame with origin ... from accessing a cross-origin frame."
-    log(`[RuntimeError] isProbablyInstanceOfNativeClass err: ${err?.stack || err}`);
-  }
+function isNativeObject(x) {
+  return fromJsIsNativeObject(x);
 }
 
 /**
@@ -601,23 +578,21 @@ function isProbablyInstanceOfNativeClass(x) {
  * is not a plain object or array.
  * @see https://linear.app/replay/issue/RUN-1592#comment-4011cec0
  */
-function isProbablyInstanceOfSpecificNativeClass(plainObject, cdpObj) {
-  return isProbablyInstanceOfNativeClass(plainObject) && 
+function isBlinkObject(plainObject, cdpObj) {
+  return isNativeObject(plainObject) && 
     cdpObj.subtype !== 'array' &&
     cdpObj.subtype !== 'typedarray' &&
     cdpObj.className !== 'Object';
 }
 
 /**
- * Check whether given object `x` is instance of class of given `target.name`,
- * and also has what looks like a native ctor.
+ * Check whether given object `x` is native object and its 
+ * class name is `target.name`.
  */
 function isInstanceOfNative(x, target) {
-  // hackfix: check if its native, and has `name` in inheritance chain
-  const name = target?.name;
-  return name &&
-    isProbablyInstanceOfNativeClass(x) &&
-    hasInProtoChain(x.constructor, name);
+  return isNativeObject(x) &&
+    target?.name &&
+    hasInProtoChain(x.constructor, target.name);
 }
 
 /**
@@ -1137,7 +1112,7 @@ ProtocolObjectPreview.prototype = {
   get pageIndex() { return 0; },
 
   get pageSize() {
-    if (isProbablyInstanceOfSpecificNativeClass(this.raw, this.cdpObj)) {
+    if (isBlinkObject(this.raw, this.cdpObj)) {
       // Don't limit native objects because in gecko, we add all native getters,
       // independent of prop limits.
       return 0;
@@ -3317,17 +3292,9 @@ extern "C" void V8RecordReplayFinishRecording();
 // associated with a given API object.
 static int GetAPIObjectIdCallback(v8::Local<v8::Object> object) {
   v8::Isolate* isolate = v8::Isolate::GetCurrent();
-  const WrapperTypeInfo* infos[] = {
-    // ScriptWrappable itself doesn't have wrapper type info, so check subclasses.
-    Node::GetStaticWrapperTypeInfo(),
-    Event::GetStaticWrapperTypeInfo(),
-    CSSStyleDeclaration::GetStaticWrapperTypeInfo()
-  };
-  for (const WrapperTypeInfo* info : infos) {
-    if (V8PerIsolateData::From(isolate)->HasInstance(info, object)) {
-      ScriptWrappable* wrappable = ToScriptWrappable(object);
-      return wrappable->RecordReplayId();
-    }
+  if (V8DOMWrapper::IsWrapper(isolate, object)) {
+    ScriptWrappable* wrappable = ToScriptWrappable(object);
+    return wrappable->RecordReplayId();
   }
   return 0;
 }
@@ -3661,6 +3628,26 @@ static void fromJsGetObjectByCdpId(
   } else {
     args.GetReturnValue().SetNull();
   }
+}
+
+/**
+ * Whether a given value is a native object.
+ * 
+ * WARNING: We currently don't have a general solution to this.
+ * This effectively only checks whether the given value is a **blink** object.
+ * If we want to generalize, we probably have to expose |v8::internal::Script::type|,
+ * which is also used by |CallSiteInfo::IsNative| for that purpose.
+ */
+static void fromJsIsNativeObject(
+    const v8::FunctionCallbackInfo<v8::Value>& args) {
+  CHECK(args.Length() == 1 && 
+        "[RuntimeError] must be called with a single value");
+
+  v8::Isolate* isolate = args.GetIsolate();
+  
+  bool result = V8DOMWrapper::IsWrapper(isolate, args[0]);
+  
+  args.GetReturnValue().Set(result);
 }
 
 /** ###########################################################################
@@ -4508,13 +4495,15 @@ void SetupRecordReplayCommands(v8::Isolate* isolate, LocalFrame* localFrame) {
   SetFunctionProperty(isolate, args, "setCommandCallback",
                       v8::FunctionCallbackRecordReplaySetCommandCallback);
 
-  // Object Management
+  // Object Util
   SetFunctionProperty(isolate, args, "fromJsMakeDebuggeeValue",
                       fromJsMakeDebuggeeValue);
   SetFunctionProperty(isolate, args, "fromJsGetArgumentsInFrame",
                       fromJsGetArgumentsInFrame);
   SetFunctionProperty(isolate, args, "fromJsGetObjectByCdpId",
                       fromJsGetObjectByCdpId);
+  SetFunctionProperty(isolate, args, "fromJsIsNativeObject",
+                      fromJsIsNativeObject);
 
   // networking
   SetFunctionProperty(isolate, args, "getCurrentNetworkRequestEvent",
