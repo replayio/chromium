82c297984732b136a101581ef3601e4fa0e7a836
Brian Hackett <bhackett1024@gmail.com>
2023-05-16 08:18:22 -0700

    rm

--- base/synchronization/waitable_event_win.cc
+++ base/synchronization/waitable_event_win.cc
@@ -56,9 +56,6 @@ void WaitableEvent::Reset() {
 }
 
 void WaitableEvent::Signal() {
-  recordreplay::Diagnostic("[RUN-1961] WaitableEvent::Signal %p %d",
-                           handle_.get(), recordreplay::PointerId(this));
-
   SetEvent(handle_.get());
 }
 
