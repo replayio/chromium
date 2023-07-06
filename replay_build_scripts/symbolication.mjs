bac2512ee7a7a3b0aea27b8adf61aeca6c1aad14
Dan Miller <dan@replay.io>
2023-07-05 19:51:42 -0700

    try this

--- replay_build_scripts/symbolication.mjs
+++ replay_build_scripts/symbolication.mjs
@@ -18,7 +18,8 @@ export async function readSymbols(file, pdbFile) {
 
     const process = spawnChecked(
       `${__dirname}\\..\\..\\lib\\llvm-pdbutil.exe`,
-      ["dump", "-symbols", pdbFile]
+      ["dump", "-symbols", pdbFile],
+      { maxBuffer: 1e100 }
     );
     // We use readline here instead of streamToLineIterator so that we can
     // keep the dependencies of this file to a minimum since this is also
