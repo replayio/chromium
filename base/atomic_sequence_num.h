6572fbd22c4dacb91c2f8d1fc0cf403fba1043d9
Domi <Domiii@users.noreply.github.com>
2023-07-07 23:43:04 +0800

    Assert main_thread_only().agent_group_schedulers and RegisterPaintTimeCallbackForCombinedCallbacks

--- base/atomic_sequence_num.h
+++ base/atomic_sequence_num.h
@@ -20,7 +20,11 @@ class AtomicSequenceNumber {
 
   // Returns an increasing sequence number starts from 0 for each call.
   // This function can be called from any thread without data race.
-  inline int GetNext() { return seq_.fetch_add(1, std::memory_order_relaxed); }
+  inline int GetNext() { 
+    int rv = seq_.fetch_add(1, std::memory_order_relaxed);
+    recordreplay::Assert("AtomicSequenceNumber::GetNext %d", rv);
+    return rv;
+  }
 
  private:
   std::atomic_int seq_{0};
