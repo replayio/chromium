3f07af7bbeb2007deb28f11a8f28913127eee3e4
Domi <Domiii@users.noreply.github.com>
2023-07-08 01:42:40 +0800

    Leak fallback_list_for_description_

--- third_party/blink/renderer/platform/fonts/font_fallback_map.cc
+++ third_party/blink/renderer/platform/fonts/font_fallback_map.cc
@@ -69,6 +69,13 @@ void FontFallbackMap::InvalidateInternal(Predicate predicate) {
 
 void FontFallbackMap::FontsNeedUpdate(FontSelector*,
                                       FontInvalidationReason reason) {
+  if (recordreplay::AreEventsDisallowed(
+          "FontFallbackMap::FontCacheInvalidated")) {
+    // Leak fallback_list_for_description_ contents to avoid divergence down the
+    // road.
+    return;
+  }
+
   AutoLockForParallelTextShaping guard(lock_);
   switch (reason) {
     case FontInvalidationReason::kFontFaceLoaded:
@@ -87,6 +94,13 @@ void FontFallbackMap::FontsNeedUpdate(FontSelector*,
 }
 
 void FontFallbackMap::FontCacheInvalidated() {
+  if (recordreplay::AreEventsDisallowed(
+          "FontFallbackMap::FontCacheInvalidated")) {
+    // Leak fallback_list_for_description_ contents to avoid divergence down the
+    // road.
+    return;
+  }
+
   AutoLockForParallelTextShaping guard(lock_);
   InvalidateAll();
 }
