27cc6596073d48eaf80a139bfa2173ca70d6c45a
Domi <Domiii@users.noreply.github.com>
2023-07-16 22:14:41 +0200

    fix auto format spasm

--- base/metrics/field_trial.cc
+++ base/metrics/field_trial.cc
@@ -742,7 +742,7 @@ void FieldTrialList::CreateTrialsFromCommandLine(const CommandLine& cmd_line,
       recordreplay::IsRecordingOrReplaying(), recordreplay::FeatureEnabled("no-field-trials"));
 
 #if !BUILDFLAG(IS_NACL) && !BUILDFLAG(IS_IOS)
-      if (cmd_line.HasSwitch(switches::kFieldTrialHandle)) {
+  if (cmd_line.HasSwitch(switches::kFieldTrialHandle)) {
     std::string switch_value =
         cmd_line.GetSwitchValueASCII(switches::kFieldTrialHandle);
     bool result = CreateTrialsFromSwitchValue(switch_value, fd_key);
