41e26cfa8d0f224724e0d8f62e0a75ce2f92c613
Dan Miller <dan@replay.io.>
2023-10-10 14:11:07 -0700

    build: don't use sudo

--- replay_build_scripts/upload_build_artifacts.mjs
+++ replay_build_scripts/upload_build_artifacts.mjs
@@ -110,7 +110,7 @@ function copyBuildFiles(srcDir, dstDir) {
 function prepareLinuxBinaries(buildId) {
   const buildArchive = `${buildId}.tar.xz`;
 
-  spawnChecked("sudo", ["rm", "-rf", "replay-chromium"], { stdio: "inherit" });
+  spawnChecked("rm", ["-rf", "replay-chromium"], { stdio: "inherit" });
 
   fs.mkdirSync("replay-chromium");
 
