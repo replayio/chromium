738db735241732c0773f99b295e65685daf2cf4f
D. Seifert <Domiii@users.noreply.github.com>
2023-10-13 17:47:29 +0800

    start experimenting

---
+++ base/record_replay_base.cc
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Record Replay Inc.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#if BUILDFLAG(IS_WIN)
+#include <windows.h>
+#endif
+
+namespace recordreplay {
+  void hello() {
+
+  }
+}  // namespace recordreplay
---
+++ base/record_replay_base.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Record Replay Inc.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_RECORD_REPLAY_BASE_H_
+#define BASE_RECORD_REPLAY_BASE_H_
+
+namespace recordreplay {
+  void hello();
+} // namespace recordreplay
+
+#endif  // BASE_RECORD_REPLAY_BASE_H_
