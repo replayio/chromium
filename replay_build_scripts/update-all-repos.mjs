99ef33620c3d6838665867418f12bbb5e32fa73c
Dan Miller <dan@replay.io>
2023-10-27 16:41:17 -0700

    cleanup

--- replay_build_scripts/update-all-repos.mjs
+++ replay_build_scripts/update-all-repos.mjs
@@ -1,5 +1,4 @@
-// This file exists so that it can be easily invoked as a script
+import { updateChromiumRepo, updateBackendRepo } from "./common.mjs";
 
-import { updateChromiumRepo } from "./common.mjs";
-
+updateBackendRepo();
 updateChromiumRepo();
--- replay_build_scripts/update-repo.mjs
+++
@@ -1,5 +0,0 @@
-import { updateChromiumRepo, updateBackendRepo } from "./common.mjs";
-
-// TODO just directly drive this from the pipeline
-updateBackendRepo();
-updateChromiumRepo();
