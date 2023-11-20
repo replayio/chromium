d1bb2a4877c8aef0a072e769f91b9dfb161c67ce
Chris Toshok <toshok@gmail.com>
2023-11-20 15:12:49 -0800

    add auth.ts

--- chrome/browser/resources/record_replay/record_replay.ts
+++ chrome/browser/resources/record_replay/record_replay.ts
@@ -14,8 +14,6 @@
 // we should also be able to add mojo bindings or use `chrome.send()`
 // and other things mentioned in webui_explainer.md.
 
-console.log("from console.log");
-console.debug("from console.debug");
-console.warn("from console.warn");
-console.error("from console.error");
-throw new Error("hello");
+import "./auth.js";
+
+console.error("in record_replay.ts after importing auth.ts");
