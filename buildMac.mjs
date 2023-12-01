442fcbbc8cdbf73fc2ceae18362a98bed1be9f0e
Dan Miller <dan@replay.io>
2023-12-01 15:27:00 -0800

    Fix mac script

--- buildMac.mjs
+++ buildMac.mjs
@@ -3,6 +3,9 @@ import fs from "fs";
 import path from "path";
 import { spawnChecked } from "./replay_build_scripts/common.mjs";
 
+const buildArm = !!process.env.REPLAY_BUILD_ARM;
+const outdir = buildArm ? "out/Release-ARM" : "out/Release";
+
 spawnChecked("node", ["build.js"], { stdio: "inherit" });
 fs.rmSync(path.join(outdir, "Replay-Chromium.app"), {
   recursive: true,
