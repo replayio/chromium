30e585941df3b0c9ef7ea39cc066e3b022fbd847
Chris Toshok <toshok@gmail.com>
2023-12-04 09:28:57 -0800

    add comment with link to linear

--- third_party/blink/renderer/core/url/dom_url.cc
+++ third_party/blink/renderer/core/url/dom_url.cc
@@ -95,6 +95,9 @@ String DOMURL::CreatePublicURL(ExecutionContext* execution_context,
 
 URLSearchParams* DOMURL::searchParams() {
   if (!search_params_) {
+    // disable events for url creation, since jit optimizations can land us
+    // here from different paths.
+    // https://linear.app/replay/issue/RUN-2324/mismatch-in-domurlsearchparams#comment-9287b64a
     recordreplay::AutoDisallowEvents disallowed("DOMURL::searchParams");
     search_params_ = URLSearchParams::Create(Url().Query(), this);
   }
