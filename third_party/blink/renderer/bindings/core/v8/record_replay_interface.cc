acbe5968e389de225d00011e36f16469571e3d8d
D. Seifert <Domiii@users.noreply.github.com>
2024-02-01 17:11:47 +0800

    [RUN-3190] Don't crash an RTP when running into bad command handling state

--- third_party/blink/renderer/bindings/core/v8/record_replay_interface.cc
+++ third_party/blink/renderer/bindings/core/v8/record_replay_interface.cc
@@ -177,6 +177,7 @@ const {
   logTrace: logTrace_,
   warning: warning_,
   fromJsIsReplayScriptAlive: isReplayScriptAlive,
+  hasDiverged,
   setCDPMessageCallback,
   sendCDPMessage: sendCDPMessageRaw,
   setCommandCallback,
@@ -460,7 +461,13 @@ function CHECK_ALIVE(message) {
   if (!isReplayScriptAlive()) {
     const err = new Error(`ReplayScript UNALIVE - ${message}`);
     err.code = CDPERROR_NOTALIVE;
-    throw err;
+    if (hasDiverged()) {
+      throw err;
+    } else {
+      // Since we don't know enough about the circumstances here yet,
+      // let's not crash an RTP for it.
+      warning(err.stack);
+    }
   }
 }
 
@@ -4016,6 +4023,11 @@ static void fromJsIsReplayScriptAlive(const v8::FunctionCallbackInfo<v8::Value>&
   args.GetReturnValue().Set(v8::Number::New(isolate, gReplayScriptsAlive));
 }
 
+static void fromJsHasDiverged(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  v8::Isolate* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(v8::Number::New(isolate, recordreplay::HasDivergedFromRecording()));
+}
+
 // Function to invoke on CDP responses and events.
 static v8::Eternal<v8::Function>* gCDPMessageCallback;
 
@@ -5629,6 +5641,8 @@ static void InitializeRecordReplayApiObjects(v8::Isolate* isolate, LocalFrame* l
   // CDP debugger functionality
   SetFunctionProperty(isolate, args, "fromJsIsReplayScriptAlive",
                       fromJsIsReplayScriptAlive);
+  SetFunctionProperty(isolate, args, "hasDiverged",
+                      fromJsHasDiverged);
   SetFunctionProperty(isolate, args, "setCDPMessageCallback",
                       SetCDPMessageCallback);
   SetFunctionProperty(isolate, args, "sendCDPMessage", SendCDPMessage);
