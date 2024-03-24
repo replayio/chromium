0400887d8040cecae58b66d6543c91aeb86920d9
Brian Hackett <bhackett1024@gmail.com>
2024-03-24 09:09:42 -0700

    build

--- third_party/blink/renderer/platform/loader/fetch/resource.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource.cc
@@ -59,6 +59,8 @@
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
+#include "base/json/json_writer.h"
+
 namespace blink {
 
 namespace {
