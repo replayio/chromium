626aa50e0b09a1f52f7e3a143970cf152abbcdda
Brian Hackett <bhackett1024@gmail.com>
2024-04-22 05:05:53 -1000

    build

--- third_party/blink/renderer/platform/loader/fetch/raw_resource.cc
+++ third_party/blink/renderer/platform/loader/fetch/raw_resource.cc
@@ -41,6 +41,8 @@
 #include "third_party/blink/renderer/platform/scheduler/public/thread_scheduler.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 
+#include "base/json/json_writer.h"
+
 namespace blink {
 
 RawResource* RawResource::FetchSynchronously(FetchParameters& params,
