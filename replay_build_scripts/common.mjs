a09106239be07e807f489e6d44e4afa51b8878b4
Brian Hackett <bhackett1024@gmail.com>
2026-03-25 20:56:13 +0000

    Add Replay build scripts (build, upload, lint, update repos)

---
+++ replay_build_scripts/common.mjs
@@ -0,0 +1,238 @@
+import { spawnSync } from "child_process";
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// NOTE(dmiller): see https://stackoverflow.com/a/62892482 for explanation
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export function spawnChecked(cmd, args, options) {
+  const prettyCmd = [cmd].concat(args).join(" ");
+  log("$" + prettyCmd);
+
+  const rv = spawnSync(cmd, args, options);
+
+  if (rv.status != 0 || rv.error) {
+    console.group(`Spawn FAILED (${rv.error || ""}) - All output:\n`);
+    const stdout = rv.stdout ? rv.stdout.toString() : "";
+    const stderr = rv.stderr ? rv.stderr.toString() : "";
+    const allOutput = `${stdout} ${stderr}`.trim();
+    if (allOutput) {
+      console.error(allOutput);
+    }
+    console.groupEnd();
+    throw new Error(`Spawned process failed with exit code ${rv.status}`);
+  }
+
+  return rv;
+}
+
+export const Platform = {
+  macOS: "macOS",
+  linux: "linux",
+  windows: "windows",
+};
+
+// Get the ID for the current platform we are running on.
+export function currentPlatform() {
+  switch (process.platform) {
+    case "darwin":
+      return Platform.macOS;
+    case "linux":
+      return Platform.linux;
+    case "win32":
+      return Platform.windows;
+    default:
+      throw new Error(`Platform ${process.platform} not supported`);
+  }
+}
+
+export function outputArchitecture() {
+  switch (currentPlatform()) {
+    case Platform.macOS:
+      if (process.env.REPLAY_BUILD_ARM) {
+        return "arm64";
+      }
+      return "x86_64";
+    case Platform.linux:
+      return "x86_64";
+    case Platform.windows:
+      return "x86_64";
+  }
+}
+
+export function assert(v, why = "") {
+  if (!v) {
+    const error = new Error(`Assertion Failed: ${why}`);
+    error.name = "AssertionFailure";
+    throw error;
+  }
+}
+
+export function log(s) {
+  console.log(s);
+}
+
+export function toNumber(str) {
+  const rv = +str;
+  if (Number.isNaN(rv) && str != "(nil)") {
+    throw new Error(`toNumber failed: "${str}"`);
+  }
+  return rv;
+}
+
+export function syncRepo(dir, treeish) {
+  log(`Syncing ${dir} to ${treeish}`);
+  try {
+    spawnChecked("git", ["fetch", "--all"], { cwd: dir, stdio: "inherit" });
+  } catch (e) {
+    // Ignore errors due to being at a detached head.
+  }
+
+  spawnChecked("git", ["reset", "--hard", treeish], {
+    cwd: dir,
+    stdio: "inherit",
+  });
+}
+
+function maybeDeleteGitLockFile(dir) {
+  const lockFile = path.join(dir, ".git", "index.lock");
+  if (fs.existsSync(lockFile)) {
+    fs.unlinkSync(lockFile);
+  }
+}
+
+export function gn() {
+  return currentPlatform() == Platform.windows ? "gn.bat" : "gn";
+}
+
+function runGnGen() {
+  spawnChecked(gn(), ["gen", "out/Release"], { stdio: "inherit" });
+}
+
+/*
+function gclient() {
+  return currentPlatform() == Platform.windows ? "gclient.bat" : "gclient";
+}
+*/
+
+function runGclientSync() {
+  // Disabled: Our fork may be too old for gclient sync to work properly.
+  //spawnChecked(gclient(), ["sync", "-D"], { stdio: "inherit" });
+}
+
+function updateRepo(repo, treeish) {
+  log(`Updating ${repo} to ${treeish}`);
+  // delete git lock file if it exists on Windows
+  if (currentPlatform() == Platform.windows) {
+    maybeDeleteGitLockFile(repo);
+  }
+
+  syncRepo(repo, treeish);
+}
+
+function enforceBackendPreludeVersion() {
+  const backend = getBackendDir();
+
+  spawnChecked("git", ["submodule", "update", "--init", "--recursive"], {
+    cwd: backend,
+    stdio: "inherit",
+  });
+
+  const preludeVersion = fs.readFileSync(
+    path.join(backend, ".preludeversion"),
+    "utf8",
+  );
+
+  const prelude = path.join(backend, "prelude");
+  updateRepo(prelude, preludeVersion);
+}
+
+export function updateBackendRepo() {
+  const backend = getBackendDir();
+  // if process.env.REPLAY_BACKEND_REV is set, use that, otherwise use the REPLAY_BACKEND_REV file
+  const rev = process.env.REPLAY_BACKEND_REV
+    ? process.env.REPLAY_BACKEND_REV
+    : fs.readFileSync("REPLAY_BACKEND_REV", "utf8").trim();
+  updateRepo(backend, rev);
+  enforceBackendPreludeVersion();
+  // create a symlink to chromium in the backend checkout
+  const chromiumRepoPath = process.cwd();
+  const chromiumPathInBackend = path.join(backend, "chromium");
+  if (fs.existsSync(chromiumPathInBackend)) {
+    fs.unlinkSync(chromiumPathInBackend);
+  }
+  fs.symlinkSync(chromiumRepoPath, chromiumPathInBackend);
+}
+
+export function updateChromiumRepo() {
+  const chromium = process.cwd();
+  const rev =
+    process.env["CHROMIUM_REVISION"] || process.env["BUILDKITE_COMMIT"];
+  updateRepo(chromium, rev);
+
+  const deps = getChromiumDeps();
+
+  syncRepo(path.join(chromium, "v8"), deps.v8);
+
+  syncRepo(path.join(chromium, "third_party", "skia"), deps.skia);
+
+  syncRepo(path.join(chromium, "third_party", "webrtc"), deps.webrtc);
+
+  syncRepo(
+    path.join(chromium, "third_party", "boringssl", "src"),
+    deps.boringssl,
+  );
+
+  runGclientSync();
+
+  runGnGen();
+}
+
+function getChromiumDeps() {
+  const text = fs.readFileSync("DEPS", "utf8");
+  let results = {
+    v8: "",
+    skia: "",
+    webrtc: "",
+    boringssl: "",
+  };
+
+  let match = /'v8_revision': '(.*?)'/.exec(text);
+  assert(match, "Could not find V8 revision");
+  results.v8 = match[1];
+
+  match = /'skia_revision': '(.*?)'/.exec(text);
+  assert(match, "Could not find skia revision");
+  results.skia = match[1];
+
+  match =
+    /'https:\/\/github.com\/replayio\/chromium-webrtc.git' \+ '@' \+ '(.*?)'/.exec(
+      text,
+    );
+  assert(match, "Could not find webrtc revision");
+  results.webrtc = match[1];
+
+  match = /'boringssl_revision': '(.*?)'/.exec(text);
+  assert(match, "Could not find boringssl revision");
+  results.boringssl = match[1];
+
+  return results;
+}
+
+export function getBackendDir() {
+  return path.resolve(
+    process.env.RECORD_REPLAY_BACKEND_DIR || path.join(__dirname, "..", ".."),
+  );
+}
+
+export function getArtifactDir() {
+  return path.join(
+    process.env.BUILDKITE_BUILD_CHECKOUT_PATH || "./",
+    "build_id",
+    currentPlatform(),
+    outputArchitecture(),
+  );
+}
