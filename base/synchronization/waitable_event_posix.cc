f52fc038cad68879d278f36bd5d81caca106a6ca
Determinator <support@replay.io>
2026-05-13 11:45:27 +0800

    [DeterminatorConflict] Deterministic pointer-id ordering in WaitMany sort in base/synchronization/waitable_event_posix.cc
    ### References
    
    * [Last downstream commit of this changeset](https://github.com/replayio/chromium/commit/8c14c3b79b4f76cb3a942e39989a846e3fc47f5b)
    * nHunks: 7
    * complexityScore: 8.7
    
    #### Upstream Changes
    - Non-semantic changes
      - Reference token spacing in `cmp_fst_addr` parameter list was reformatted from `unsigned> &a` to `unsigned>& a` (and similarly for `b`). Purely cosmetic; no symbols in the dependency set were modified.
    
    #### Original Diff
    ```diff
    @@ -245,0 +263,8 @@ cmp_fst_addr(const std::pair<WaitableEvent*, unsigned> &a,
    +  // When recording/replaying, sort by the pointer ID to get a consistent order.
    +  // Note that pointer IDs will not be present for unordered waitable events.
    +  if (recordreplay::IsRecordingOrReplaying("pointer-ids")) {
    +    int ida = recordreplay::PointerId(a.first);
    +    int idb = recordreplay::PointerId(b.first);
    +    if (ida && idb)
    +      return ida < idb;
    +  }
    ```

--- base/synchronization/waitable_event_posix.cc
+++ base/synchronization/waitable_event_posix.cc
@@ -233,6 +233,14 @@ bool WaitableEvent::TimedWaitImpl(TimeDelta wait_delta) {
 static bool  // StrictWeakOrdering
 cmp_fst_addr(const std::pair<WaitableEvent*, unsigned>& a,
              const std::pair<WaitableEvent*, unsigned>& b) {
+  // When recording/replaying, sort by the pointer ID to get a consistent order.
+  // Note that pointer IDs will not be present for unordered waitable events.
+  if (recordreplay::IsRecordingOrReplaying("pointer-ids")) {
+    int ida = recordreplay::PointerId(a.first);
+    int idb = recordreplay::PointerId(b.first);
+    if (ida && idb)
+      return ida < idb;
+  }
   return a.first < b.first;
 }
 
