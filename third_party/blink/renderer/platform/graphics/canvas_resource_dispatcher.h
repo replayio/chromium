a0755d7a3c9a62e4521b16368a1c2467ada25ad2
Determinator <support@replay.io>
2026-05-13 21:35:04 +0800

    [DeterminatorConflict] Use unique_leaky_ptr for animation_power_mode_voter_
    ### Non-semantic Changes
    * `animation_power_mode_voter_` field re-applied with `recordreplay::unique_leaky_ptr` between `client_` and `task_runner_`.
      * `components/power_scheduler/power_mode_voter.h` include restored for the field's type.
    
    ### References
    
    * [Last downstream commit of this changeset](https://github.com/replayio/chromium/commit/dc485c69fc6cf200f9ebdd2f101d27b9ba40faf2)
    * nHunks: 2
    * complexityScore: 3
    
    #### Upstream Changes
    - Non-semantic changes
      - The adjacent field `client_` was retyped from `CanvasResourceDispatcherClient*` to `raw_ptr<CanvasResourceDispatcherClient>`, shifting context around the `animation_power_mode_voter_` declaration. No symbol in the ChangeGroup's dependency set was modified.
    
    #### Original Diff
    ```diff
    @@ -158 +159 @@ class PLATFORM_EXPORT CanvasResourceDispatcher
    -  std::unique_ptr<power_scheduler::PowerModeVoter> animation_power_mode_voter_;
    +  recordreplay::unique_leaky_ptr<power_scheduler::PowerModeVoter> animation_power_mode_voter_;
    ```

--- third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.h
@@ -12,6 +12,7 @@
 #include "base/record_replay.h"
 #include "base/task/single_thread_task_runner.h"
 #include "cc/paint/paint_flags.h"
+#include "components/power_scheduler/power_mode_voter.h"
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "components/viz/common/resources/resource_id.h"
 #include "components/viz/common/surfaces/parent_local_surface_id_allocator.h"
@@ -177,6 +178,8 @@ class PLATFORM_EXPORT CanvasResourceDispatcher
   viz::BeginFrameAck current_begin_frame_ack_;
 
   raw_ptr<CanvasResourceDispatcherClient> client_;
+
+  recordreplay::unique_leaky_ptr<power_scheduler::PowerModeVoter> animation_power_mode_voter_;
 
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner>
