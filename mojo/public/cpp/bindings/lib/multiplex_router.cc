4cc82208479bcd0892c6855c3d4fca129759dcc7
Determinator <support@replay.io>
2026-05-13 16:15:42 +0800

    [DeterminatorConflict] Register MultiplexRouter pointer
    ### References
    
    * [Last downstream commit of this changeset](https://github.com/replayio/chromium/commit/d102a598f6366926c0fdafa4ea94f9849b1f255c)
    * nHunks: 3
    * complexityScore: 4.266666666666667
    
    #### Upstream Changes
    - Non-semantic changes
      - Style change: braces added around the single-statement `if (config_ == MULTI_INTERFACE)` body containing `lock_.emplace()`. This only shifts context around the ChangeGroup's insertion site.
    
    #### Original Diff
    ```diff
    @@ -385,0 +396,2 @@ MultiplexRouter::MultiplexRouter(
    +  recordreplay::RegisterPointer("MultiplexRouter", this);
    +
    ```

--- mojo/public/cpp/bindings/lib/multiplex_router.cc
+++ mojo/public/cpp/bindings/lib/multiplex_router.cc
@@ -424,6 +424,8 @@ MultiplexRouter::MultiplexRouter(
                  primary_interface_name),
       control_message_handler_(this),
       control_message_proxy_(&connector_) {
+  recordreplay::RegisterPointer("MultiplexRouter", this);
+
   if (config_ == MULTI_INTERFACE) {
     lock_.emplace();
   }
