3709d9896bf7f837e31bb327106ba517dffb916d
Determinator <support@replay.io>
2026-05-13 14:57:13 +0800

    [DeterminatorConflict] Deterministic ordering for task queue voters map
    ### Non-semantic Changes
    * Multi-line formatting of the third std::map template argument to align with upstream's reformatted alias.
    
    ### References
    
    * [Last downstream commit of this changeset](https://github.com/replayio/chromium/commit/930c6e4554d68da4d90017d60e5bd4980cc04e6d)
    * nHunks: 4
    * complexityScore: 5
    
    #### Upstream Changes
    - Non-semantic changes
      - The TaskQueueVoterMap alias gained an ALLOW_DISCOURAGED_TYPE("TODO(crbug.com/1404327)") annotation and was reformatted; the std::map template arguments referenced by the downstream lines are otherwise unchanged.
    
    #### Original Diff
    ```diff
    @@ -746 +749,2 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
    -      std::unique_ptr<base::sequence_manager::TaskQueue::QueueEnabledVoter>>;
    +      std::unique_ptr<base::sequence_manager::TaskQueue::QueueEnabledVoter>,
    +      recordreplay::CompareRefptrByPointerId<scoped_refptr<MainThreadTaskQueue>>>;
    ```

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
@@ -737,7 +737,9 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
   using TaskQueueVoterMap ALLOW_DISCOURAGED_TYPE("TODO(crbug.com/1404327)") =
       std::map<scoped_refptr<MainThreadTaskQueue>,
                std::unique_ptr<
-                   base::sequence_manager::TaskQueue::QueueEnabledVoter>>;
+                   base::sequence_manager::TaskQueue::QueueEnabledVoter>,
+               recordreplay::CompareRefptrByPointerId<
+                   scoped_refptr<MainThreadTaskQueue>>>;
 
   TaskQueueVoterMap task_runners_;
 
