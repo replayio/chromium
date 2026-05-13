7c2e1ccd3b552291770e027dfdeb7929fa6e69ef
Determinator <support@replay.io>
2026-05-14 03:22:22 +0800

    [DeterminatorConflict] Declare RecordReplayForRecording virtual method on SiteInstance
    ### Non-semantic Changes
    * Insertion site
      * Anchor `IsGuest` moved to `SecurityPrincipal`
      * Declaration relocated to follow `IsSameSiteWithURL` on `SiteInstance`
    
    ### References
    
    * [Last downstream commit of this changeset](https://github.com/replayio/chromium/commit/7bcb2a985910f983a00d3b97952fb2ba72bdc505)
    * nHunks: 1
    * complexityScore: 2
    
    #### Upstream Changes
    - [moveFile] [SiteInstance::IsGuest → content/public/browser/security_principal.h:SecurityPrincipal::IsGuest] IsGuest was moved from SiteInstance to the new SecurityPrincipal interface; the anchor method preceding the downstream insertion no longer exists on SiteInstance.
    - Non-semantic changes
      - None.
    
    #### Original Diff
    ```diff
    @@ -190,0 +191,3 @@ class CONTENT_EXPORT SiteInstance : public base::RefCounted<SiteInstance> {
    +  // Returns true if this site instance is for a site that should be recorded.
    +  virtual bool RecordReplayForRecording() = 0;
    +
    ```

--- content/public/browser/site_instance.h
+++ content/public/browser/site_instance.h
@@ -221,6 +221,9 @@ class CONTENT_EXPORT SiteInstance : public base::RefCounted<SiteInstance> {
   // assignment.
   virtual bool IsSameSiteWithURL(const GURL& url) = 0;
 
+  // Returns true if this site instance is for a site that should be recorded.
+  virtual bool RecordReplayForRecording() = 0;
+
   // Returns how this SiteInstance was assigned to a renderer process the most
   // recent time that such an assignment was done. This allows the content
   // embedder to collect metrics on how renderer process starting or reuse
