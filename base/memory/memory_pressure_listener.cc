be2c550029d70dd4e46a367f938414e97b17c814
Determinator <support@replay.io>
2026-05-13 16:55:56 +0800

    [DeterminatorConflict] include base/record_replay.h in memory_pressure_listener.cc
    ### Non-semantic Changes
    * `#include "base/record_replay.h"` placed in alphabetical order between `base/notreached.h` and `base/task/sequenced_task_runner.h`
      * Original downstream anchor include (`base/trace_event/base_tracing.h`) no longer exists upstream
    
    ### References
    
    * [Last downstream commit of this changeset](https://github.com/replayio/chromium/commit/25b1bbc8d94bace74589c1162cfb2b6d807c5a2f)
    * nHunks: 3
    * complexityScore: 4
    
    #### Upstream Changes
    - Non-semantic changes
      - Include-list churn around the downstream `#include "base/record_replay.h"` line. The block of includes was rewritten: removed `base/observer_list.h`, `base/observer_list_threadsafe.h`, `base/threading/sequenced_task_runner_handle.h`, `base/trace_event/base_tracing.h`; added `base/check_op.h`, `base/debug/leak_annotations.h`, `base/functional/bind.h`, `base/logging.h`, `base/memory/memory_pressure_listener_registry.h`, `base/memory_coordinator/utils.h`, `base/notreached.h`, `base/task/sequenced_task_runner.h`, `base/task/single_thread_task_runner.h`, `base/trace_event/interned_args_helper.h`, `base/trace_event/memory_dump_manager.h`, `base/trace_event/memory_pressure_level_proto.h`, `base/trace_event/trace_event.h`. This shifts the position of the downstream include but does not affect any symbol it depends on.
    
    #### Original Diff
    ```diff
    @@ -12,0 +13 @@
    +#include "base/record_replay.h"
    ```

--- base/memory/memory_pressure_listener.cc
+++ base/memory/memory_pressure_listener.cc
@@ -14,6 +14,7 @@
 #include "base/memory/memory_pressure_listener_registry.h"
 #include "base/memory_coordinator/utils.h"
 #include "base/notreached.h"
+#include "base/record_replay.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/trace_event/interned_args_helper.h"
