ae13861258e5a8eb1f3b9ed83a772ca6a52b91f5
Determinator <support@replay.io>
2026-05-13 21:00:02 +0800

    [DeterminatorConflict] include record_replay.h in mojo/core/watcher_dispatcher.h
    ### Non-semantic Changes
    * `#include "base/record_replay.h"` placed after `base/memory/scoped_refptr.h` within reorganized include block.
    
    ### References
    
    * [Last downstream commit of this changeset](https://github.com/replayio/chromium/commit/fa58e4bf8de14305abc975de301736e851de1b1c)
    * nHunks: 2
    * complexityScore: 3
    
    #### Upstream Changes
    - Non-semantic changes
      - Include block reorganized: base/memory/ref_counted.h was replaced with base/memory/raw_ptr.h and base/memory/scoped_refptr.h, shifting the position where the downstream #include "base/record_replay.h" line is anchored.
    
    #### Original Diff
    ```diff
    @@ -13,0 +14 @@
    +#include "base/record_replay.h"
    ```

--- mojo/core/watcher_dispatcher.h
+++ mojo/core/watcher_dispatcher.h
@@ -12,6 +12,7 @@
 #include "base/containers/flat_map.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/record_replay.h"
 #include "base/synchronization/lock.h"
 #include "mojo/core/dispatcher.h"
 #include "mojo/core/handle_signals_state.h"
