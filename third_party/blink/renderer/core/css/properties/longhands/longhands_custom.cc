f6086f53d7561ea700a5e307c73a1ba1ebd8620d
Determinator <support@replay.io>
2026-05-14 18:10:33 +0800

    [DeterminatorConflict] Re-apply recordreplay::Assert in Filter::ApplyValue
    ### Non-semantic Changes
    * recordreplay::Assert at the start of Filter::ApplyValue
      * identical to original downstream line
      * surrounding upstream signature/StyleBuilder context preserved as-is
    
    ### References
    
    * [Last downstream commit of this changeset](https://github.com/replayio/chromium/commit/fc58ed82dd2df40844ccf8c51647f8d166b3dd45)
    * nHunks: 1
    * complexityScore: 2
    
    #### Upstream Changes
    - [transform] [Filter::ApplyValue → third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc:Filter::ApplyValue] Signature gained a ValueMode parameter.
    - [transform] [StyleResolverState::Style → third_party/blink/renderer/core/css/resolver/style_resolver_state.h:StyleResolverState::StyleBuilder] Renamed/replaced by StyleResolverState::StyleBuilder() at the call site immediately following the inserted Assert.
    
    #### Original Diff
    ```diff
    @@ -2757,0 +2758 @@ void Filter::ApplyValue(StyleResolverState& state,
    +  recordreplay::Assert("[RUN-2424-3227] longhands::Filter::ApplyValue");
    ```

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -4530,6 +4530,7 @@ const CSSValue* Filter::CSSValueFromComputedStyleInternal(
 void Filter::ApplyValue(StyleResolverState& state,
                         const CSSValue& value,
                         ValueMode) const {
+  recordreplay::Assert("[RUN-2424-3227] longhands::Filter::ApplyValue");
   state.StyleBuilder().SetFilter(StyleBuilderConverter::ConvertFilterOperations(
       state, value, PropertyID()));
 }
