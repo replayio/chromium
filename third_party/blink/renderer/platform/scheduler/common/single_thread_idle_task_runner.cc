1b2409103edbd849e47d361f7fd281c93c2bc6fe
Determinator <support@replay.io>
2026-05-14 21:06:50 +0800

    [DeterminatorConflict] Re-add base/record_replay.h include in single_thread_idle_task_runner.cc
    ### Non-semantic Changes
    * Include placement
      * Alphabetized within the existing include block instead of separated by a blank line before the namespace
    
    ### References
    
    * [Last downstream commit of this changeset](https://github.com/replayio/chromium/commit/3755c3fe85aaad421dcd32c77793898471222edb)
    * nHunks: 1
    * complexityScore: 2
    
    #### Upstream Changes
    - Non-semantic changes
      - Position of the downstream `#include "base/record_replay.h"` shifted because a `base::CallbackCancellationTraits` specialization for `SingleThreadIdleTaskRunner::RunTaskDecltype` was added above the `namespace blink` block, and the namespace declaration style was collapsed from nested `namespace blink { namespace scheduler {` into `namespace blink::scheduler {`. None of these symbols are referenced by the ChangeGroup diff.
    
    #### Original Diff
    ```diff
    @@ -10,0 +11,2 @@
    +#include "base/record_replay.h"
    +
    ```

--- third_party/blink/renderer/platform/scheduler/common/single_thread_idle_task_runner.cc
+++ third_party/blink/renderer/platform/scheduler/common/single_thread_idle_task_runner.cc
@@ -5,6 +5,7 @@
 #include "third_party/blink/renderer/platform/scheduler/common/single_thread_idle_task_runner.h"
 
 #include "base/location.h"
+#include "base/record_replay.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/trace_event/trace_event.h"
 
