db312eade730b37289d80cf3ff0b6256f4090103
Determinator <support@replay.io>
2026-05-14 15:24:13 +0800

    [DeterminatorConflict] Re-apply RecordReplayId declaration in tree_scope.h
    ### Non-semantic Changes
    * `int RecordReplayId() const;` declaration placement
      * Position shifted below newly-introduced upstream members (customElementRegistry, SetCustomElementRegistry, IsWaitingForScopedRegistry, ElementForHitTest)
    
    ### References
    
    * [Last downstream commit of this changeset](https://github.com/replayio/chromium/commit/3ca51309474513873a38f718b5eb59f43503c7e8)
    * nHunks: 1
    * complexityScore: 2
    
    #### Upstream Changes
    - Non-semantic changes
      - Added new member declarations (customElementRegistry, SetCustomElementRegistry, IsWaitingForScopedRegistry, ElementForHitTest) above the downstream insertion point, shifting its position but not affecting the RecordReplayId symbol.
    
    #### Original Diff
    ```diff
    @@ -155,0 +156,2 @@ class CORE_EXPORT TreeScope : public GarbageCollectedMixin {
    +  int RecordReplayId() const;
    +
    ```

--- third_party/blink/renderer/core/dom/tree_scope.h
+++ third_party/blink/renderer/core/dom/tree_scope.h
@@ -196,6 +196,8 @@ class CORE_EXPORT TreeScope : public GarbageCollectedMixin {
   // should be dispatched to.
   Element* ElementForHitTest(Node*, HitTestPointType) const;
 
+  int RecordReplayId() const;
+
  protected:
   TreeScope(ContainerNode&, Document&);
   explicit TreeScope(Document&);
