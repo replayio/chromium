0699381d0c4c64f3ec135ce356250c13a1ea29aa
Determinator <support@replay.io>
2026-05-14 19:43:26 +0800

    [DeterminatorConflict] Trace replay_strong_element_instances_ in SVGElementRareData
    ### Non-semantic Changes
    * `visitor->Trace(replay_strong_element_instances_)` placed after `visitor->Trace(resource_target_)` due to upstream shift.
    
    ### References
    
    * [Last downstream commit of this changeset](https://github.com/replayio/chromium/commit/0384e1434bcd9a72eb793cd354dd0b2750e3b225)
    * nHunks: 1
    * complexityScore: 2
    
    #### Upstream Changes
    - Non-semantic changes
      - A new visitor->Trace(resource_target_) line was added inside SVGElementRareData::Trace, and surrounding methods (EnsureResourceTarget, HasResourceTarget, override_computed_style_ trace) shifted the position of the downstream insertion.
    
    #### Original Diff
    ```diff
    @@ -72,0 +73 @@ void SVGElementRareData::Trace(Visitor* visitor) const {
    +  visitor->Trace(replay_strong_element_instances_);
    ```

--- third_party/blink/renderer/core/svg/svg_element_rare_data.cc
+++ third_party/blink/renderer/core/svg/svg_element_rare_data.cc
@@ -91,6 +91,7 @@ void SVGElementRareData::Trace(Visitor* visitor) const {
   visitor->Trace(resource_client_);
   visitor->Trace(smil_animations_);
   visitor->Trace(resource_target_);
+  visitor->Trace(replay_strong_element_instances_);
 }
 
 AffineTransform* SVGElementRareData::AnimateMotionTransform() {
