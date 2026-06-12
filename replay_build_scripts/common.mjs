84b2edf6c624a6f175595edc7a03b6f5f44fac08
D. Seifert <Domiii@users.noreply.github.com>
2026-06-13 00:46:30 +0800

    reclient fix

--- replay_build_scripts/common.mjs
+++ replay_build_scripts/common.mjs
@@ -123,6 +123,14 @@ function runGclientSync() {
   //spawnChecked(gclient(), ["sync", "-D"], { stdio: "inherit" });
 }
 
+function runConfigureReclient() {
+  spawnChecked(
+    "python3",
+    ["third_party/reclient_configs/configure_reclient.py", "--src_dir=."],
+    { stdio: "inherit" },
+  );
+}
+
 function updateRepo(repo, treeish) {
   log(`Updating ${repo} to ${treeish}`);
   // delete git lock file if it exists on Windows
@@ -187,6 +195,8 @@ export function updateChromiumRepo() {
   );
 
   runGclientSync();
+
+  runConfigureReclient();
 
   runGnGen();
 }
